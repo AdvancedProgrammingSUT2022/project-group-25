ca670baeefe5941f064bd169c414825cff9246ae
lioneraV2002 <kamyar_maham1381@yahoo.com>
2022-04-25 15:11:43 +0430

    controller and view classes added, classes modified, menus initializes, gson dependency added, model classes initiated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import view.LoginMenu;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        LoginMenu loginMenu = new LoginMenu();
+        loginMenu.run();
+    }
 }
---
+++ src/main/java/controller/ProfileMenuController.java
@@ -0,0 +1,4 @@
+package controller.menuControllers;
+
+public class ProfileMenuController {
+}
