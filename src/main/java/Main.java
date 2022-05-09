66b57c1859c6da71f6ab22c67dae0a4188b1d757
lioneraV2002 <kamyar_maham1381@yahoo.com>
2022-05-09 20:55:25 +0430

    ....

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import model.DataBase;
-import view.LoginMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        DataBase.readUsers();
-        new LoginMenu().run();
-    }
-}
