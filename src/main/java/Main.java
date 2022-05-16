b76426fe1cafd5c5844f1801501a94baad41056a
lioneraV2002 <kamyar_maham1381@yahoo.com>
2022-05-16 23:29:00 +0430

    new startegy

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import view.LoginMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        UtilityClass.getInstance().readUsers();
-        LoginMenu.getInstance().run();
-    }
-}
